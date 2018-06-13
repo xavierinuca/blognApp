ca469e1d1b3ee9d0f81fb5158fea7c90ad66692c
Xavier Inuca <bryanxavier95@gmail.com>
2018-06-13 02:43:18 -0500

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NuevoBlogComponent } from './componentes/nuevo-blog/nuevo-blog.component';
+import { AdminComponent } from './componentes/admin/admin.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { NotFoungComponent } from './componentes/not-foung/not-foung.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { DetailsComponent } from './componentes/details/details.component';
+import { EditComponent } from './componentes/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    NuevoBlogComponent,
+    AdminComponent,
+    AboutComponent,
+    NotFoungComponent,
+    NotFoundComponent,
+    DetailsComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
